bd8f4ee8636e2d114543ac42af336d8410344eca
SRHARIH <sriram.hariharan@mercedes-benz.com>
2023-06-18 03:52:19 +0530

    prod final

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 ReactDOM.render(
   <React.StrictMode>
     {/* <BrowserRouter> */}
